a09cd0cc91a665e3f3d65093d459d66a15756ee9
Tejas <tejas.sriv.44@gmail.com>
2025-07-08 09:54:21 +0530

    Abstracting the db credentials

--- school-management-app/src/controllers/db.py
+++ school-management-app/src/controllers/db.py
@@ -1,9 +1,25 @@
 import mysql.connector
+import sys
+import os
+
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
+from config import DB_CONFIG
 
 def get_connection():
-    return mysql.connector.connect(
-        host="localhost",
-        user="root",
-        password="Ambrane@#3344",
-        database="School"
-    )
+    try:
+        conn = mysql.connector.connect(
+            host=DB_CONFIG['host'],
+            user=DB_CONFIG['user'],
+            password=DB_CONFIG['password'],
+            database=DB_CONFIG['database']
+        )
+        return conn
+    except mysql.connector.Error as err:
+        if err.errno == mysql.connector.errorcode.ER_ACCESS_DENIED_ERROR:
+            print("Something is wrong with your user name or password")
+        elif err.errno == mysql.connector.errorcode.ER_BAD_DB_ERROR:
+            print("Database does not exist")
+        else:
+            print(err)
+        raise
